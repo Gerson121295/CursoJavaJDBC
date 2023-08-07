8402bbf8a348628d071e027c5f83bdafa988d62f
Gerson121295 <Gerson21millones@gmail.com>
2023-08-06 20:20:21 -0600

    2.2 - Listado con Statement. Muestra los productos de la BD en la vista.

---
+++ src/main/java/com/alura/jdbc/controller/ProductoController.java
@@ -0,0 +1,65 @@
+package com.alura.jdbc.controller;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class ProductoController {
+	
+	public void modificar(String nombre, String descripcion, Integer id) {
+		// TODO
+	}
+
+	public void eliminar(Integer id) {
+		// TODO
+	}
+
+	public List<Map<String, String>> listar() throws SQLException {
+		
+		Connection con = DriverManager.getConnection(
+				//Conexion a la BD de MySQL
+				"jdbc:mysql://localhost/control_de_stock?useTimeZone=true&serverTimeZone=UTC",
+				"root", "admin");
+		
+		Statement statement = con.createStatement(); //con este objeto podremos declarar el query para la consulta a la BD
+		
+		statement.execute("SELECT id, nombre, descripcion, cantidad FROM producto");
+		//execute devuelve un boolean  para indicar si el resultado del statement es un listado como el select devuelve un true, 
+		// si no retorna un false que seria un update, delete o insert.     
+		
+		
+		ResultSet resultSet = statement.getResultSet(); //Estado de resultado
+		
+		List<Map<String, String>> resultado = new ArrayList<>();
+		
+		while(resultSet.next()) {//Para poder ver el contenido para agregarlo al listado del resultado
+			Map<String, String> fila = new HashMap<>();
+			fila.put("id", String.valueOf(resultSet.getInt("id")));
+			fila.put("nombre", resultSet.getString("nombre"));
+			fila.put("descripcion", resultSet.getString("descripcion"));
+			fila.put("cantidad", String.valueOf(resultSet.getInt("cantidad")));
+			
+			resultado.add(fila);
+		}
+		
+		
+		con.close(); //cerramos la conexion
+		
+		return resultado; // Devolvemos la informacion
+	}
+
+    public void guardar(Object producto) {
+		// TODO
+	}
+    
+
+}
+
+
+
