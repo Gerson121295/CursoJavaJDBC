266cd0ab59daebf81c3225b850772ddb9fe4d44b
Gerson121295 <Gerson21millones@gmail.com>
2023-08-07 16:33:43 -0600

    5.0 - Capa de Persistencia con DAO

---
+++ src/main/java/com/alura/jdbc/modelo/Producto.java
@@ -0,0 +1,14 @@
+package com.alura.jdbc.modelo;
+
+
+//Representa la tabla de producto de la BD
+public class Producto {
+	
+	private Integer id;
+	private String nombre;
+	private String descripcion;
+	private Integer cantidad;
+	
+		
+
+}
