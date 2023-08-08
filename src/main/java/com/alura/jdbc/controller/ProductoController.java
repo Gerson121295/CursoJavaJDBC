88cef209b87ec801b9ceef68b77e2bb5abe8d8f6
Gerson121295 <Gerson21millones@gmail.com>
2023-08-08 10:25:44 -0600

    5.2 - DAO con INSERT del producto

--- src/main/java/com/alura/jdbc/controller/ProductoController.java
+++ src/main/java/com/alura/jdbc/controller/ProductoController.java
@@ -10,6 +10,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import com.alura.jdbc.dao.ProductoDAO;
 import com.alura.jdbc.factory.ConnectionFactory;
 import com.alura.jdbc.modelo.Producto;
 
@@ -144,205 +145,13 @@ public class ProductoController {
 	
    // public void guardar(Map<String, String> producto) throws SQLException { //definicion de recorrido por Map <String, String)
     	public void guardar(Producto producto) throws SQLException { // Se le envia el objeto Producto
-    	//Funciona este Bloque1.
-    	//Connection con = new ConnectionFactory().recuperaConexion();
-
-		//Opcion de Insert Para evitar SQL Injection utilizando PreparedStatement: ingresar:  Mouse'  con el signo '  genera error por en en SQL las comillas simples '' señalan un String 
-		//COn esto pasamos la seguridad al JDBC
-		//El PreparedStatement se encarga de normalizar el texto ingresado como texto común. SI se agrega un ' el lo agrega asi.
-	/*
-		PreparedStatement statement = con.prepareStatement("INSERT INTO PRODUCTO (nombre, descripcion, cantidad)"
-					+ " VALUES (?, ?, ?)",
-					Statement.RETURN_GENERATED_KEYS);
-					statement.setString(1, producto.get("NOMBRE"));
-					statement.setString(2, producto.get("DESCRIPCION"));
-					statement.setInt(3, Integer.valueOf(producto.get("CANTIDAD")));					
-					statement.execute();
-		
-	*/
-		
-	//Bloque de codigo anterior pero con Regla de negocio: 
-	//Codigo para cumplir la regla de negocio: Soporta 50 cajas de un producto por registro. Si se ingresan la caontidad de 100 productos, entonces automaticamente se haran 2 registros del mismo producto con 50 cada uno.
-    	
-    	/*//Codigo Utilizado para: public void guardar(Map<String, String> producto)
-    	String nombre = producto.get("NOMBRE");
-    	String descripcion = producto.get("DESCRIPCION");
-    	Integer cantidad = Integer.valueOf(producto.get("CANTIDAD"));
-    	Integer maximoCantidad = 50;
-    	*/
-    
-    
-    //codigo para: public void guardar(Producto producto) ahora es por objeto y se necesitará los Getters and Setters
-    /*   //Logica de negocio: Permite registrar solo 50 productos.
-    		var nombre = producto.getNombre();
-        	var descripcion = producto.getDescripcion();
-        	var cantidad = producto.getCantidad();
-        	final var maximoCantidad = 50;
-        */	
-    	
-    	//Connection con = new ConnectionFactory().recuperaConexion();
-    	
-    	//Para version 9 > java - Agregando el try-with-resource para asegurar que se ci
-    	ConnectionFactory factory = new ConnectionFactory();
-    	
-    	//para el try-with-resources se agrego final
-    	final Connection con = factory.recuperaConexion();
-    	
-    	try(con){//para el try-with-resources se agrego el try(con)
-	    	//Comando para tomar el control de la transaccion: Transaction Para asegurar que se inserten todos los dato o no se inserte ninguno.	
-	    	con.setAutoCommit(false); //Para tomar el control de la transaccion
-	    	
-	    	//para el try-with-resources se agrego final 
-	    	final PreparedStatement statement = con.prepareStatement("INSERT INTO PRODUCTO (nombre, descripcion, cantidad)"
-							+ " VALUES (?, ?, ?)",
-							Statement.RETURN_GENERATED_KEYS);
-					
-		//Logica para cumplir la regla de negocio
-		try(statement){ //para el try-with-resources se agrego el try(statement)
-			//try { //Se elimino este try debido a que ya se tiene el que recibe statement del try-with-resource.
-				
-				//do {	//Logica de negocio: Permite registrar solo 50 productos.
-							 
-					//int cantidadParaGuardar = Math.min(cantidad, maximoCantidad); //Logica de negocio: Permite registrar solo 50 productos. // si cantidad = 100 y maximoCantidad = 50 El valor minimo sera: 50.  otro Ejemp: si cantidad = 40, maxCant = 50, el valor a guardar es: 40.
-					
-					//ejecutaRegistro(nombre, descripcion, cantidadParaGuardar, statement);	//Funcion ejecutarRegistro: A la Funcion usando Map anterior se necesitaba enviarle las variables que almacenaba la data.
-					
-					ejecutaRegistro(producto, statement);	//Funcion ejecutarRegistro: A la Funcion Se le envia el objeto producto.
-					
-					//cantidad -= maximoCantidad; //Logica de negocio: Permite registrar solo 50 productos. // cantidad = cantidad - maximoCantidad //para asegurar que se ingresen 50 registros si es mas de 50, el resto se ingresa en el siguiente registro.
-					
-				//}while(cantidad > 0);  	//Logica de negocio: Permite registrar solo 50 productos.
-						
-				con.commit(); //para garantizar que todos los comandos del ciclo while hallan sido ejecutados correctamente.
-				System.out.println("Commit");	
-				
-			}catch(Exception e) { //aqui cerraba el try que se elimino arriba
-				con.rollback(); //Si hubo un erro y no se ejecutaron todas las lineas de codigo, se revierten las que se ejecutaron. Por lo tanto no habra registros, Es como no paso nada.	
-				System.out.println("Rollback");
-			}
-		
-		} //llave del try
-    	// } //llave del try, se elimino debido a que se elimino un try arriba.
-		
-    	//ya no es necesario cerrar la conexion manual debido a que ya fue cerrada con el try-with-resource
-		//statement.close(); //Cerrar el PreparedStatement
-		//con.close(); //cerrar la conexion	
-		
-    	
-		
-		/* //Opcion: no segura para insert vulnerable a SQL Injection
-		 
-		  //Logica de Insert con Statement	
-		Statement statement = con.createStatement();
-		
-		String sqlInsert = "INSERT INTO PRODUCTO(nombre, descripcion, cantidad) " //No se escribe el id porque es auto_incremente en la BD
-				+ " VALUES('" + producto.get("NOMBRE") + "', '" //se agrego comillas simples debido a que es un String el campo nombre, comillas simples '' en BD señala un String y en java son las comillas dobles "" para el String
-				+ producto.get("DESCRIPCION") + "', "
-				+ producto.get("CANTIDAD") + ")";
-		
-		System.out.println(sqlInsert);
-		statement.execute(
-				sqlInsert,
-				Statement.RETURN_GENERATED_KEYS);
-		*/
-		
-		/*//Opcion: no segura para insert vulnerable a SQL Injection
-		statement.execute("INSERT INTO PRODUCTO(nombre, descripcion, cantidad) " //No se escribe el id porque es auto_incremente en la BD
-					+ " VALUES('" + producto.get("NOMBRE") + "', '" //se agrego comillas simples debido a que es un String el campo nombre, comillas simples '' en BD señala un String y en java son las comillas dobles "" para el String
-					+ producto.get("DESCRIPCION") + "', "
-					+ producto.get("CANTIDAD") + ")", Statement.RETURN_GENERATED_KEYS); //")", cerrar el parentesis de la Query //Statement.RETURN_GENERATED_KEYS - Cuando se ejecuta un insert (se incrementa el id) obtiene el id generado con insert 
-		*/
-		
-		
-		/*//Parte  siguiente del Bloque1
-		 //Funciona pero esta parte fue extraida e ingresada en el metodo ejecutaRegistro
-		
-		ResultSet resultSet = statement.getGeneratedKeys(); // Resultado de los id generados con la ejecucion de la Query
-		
-		while(resultSet.next()) {//while para obtener el valor del id generado
-				System.out.println(
-						String.format(
-							"Fue insertado el producto de ID %d",
-							 resultSet.getInt(1)));//posicion 1 para saber cual id fue generado										
-		}
-		*/
-		
-    }
-
-
-	//private void ejecutaRegistro(String nombre, String descripcion, Integer cantidad, PreparedStatement statement)  //metodo anterior usando Map se le envia las variables que tendria la data.
-    private void ejecutaRegistro(Producto producto, PreparedStatement statement)  //Recibe el objeto Producto producto que contiene sus atributos.
+    		//Se refactorizo el codigo y la logica se traslado a ProductoDAO
     		
-			throws SQLException {
-		
-		//if(cantidad < 50) { //Lanzar un error cuando sean menor a 50 no se ejecutaria el codigo.
-		//	throw new RuntimeException("Ocurrio un error");
-		//}
-		/*codigo para -  private void ejecutaRegistro(String nombre, String descripcion, Integer cantidad, PreparedStatement statement)
-		statement.setString(1, nombre);							
-		statement.setString(2, descripcion);								
-		statement.setInt(3, cantidad);													
-		statement.execute();
-		*/
+    		ProductoDAO productoDAO =  new ProductoDAO(new ConnectionFactory().recuperaConexion());  			
+    		productoDAO.guardar(producto);
     	
+    	}	
     	
-    	// codigo para -  private void ejecutaRegistro(Producto producto, PreparedStatement statement)
-		statement.setString(1, producto.getNombre());							
-		statement.setString(2, producto.getDescripcion());								
-		statement.setInt(3, producto.getCantidad());													
-		statement.execute();
-		
- 
-		/*
-		//Funciona: Codigo simple sin try-with-resources para asegurar de cerrar la conexion
-		ResultSet resultSet = statement.getGeneratedKeys(); // Resultado de los id generados con la ejecucion de la Query
-		
-		while(resultSet.next()) {//while para obtener el valor del id generado
-				System.out.println(
-						String.format(
-							"Fue insertado el producto de ID %d",
-							 resultSet.getInt(1)));//posicion 1 para saber cual id fue generado										
-		}
-		
-		resultSet.close(); // Cerrar el resultSet
-		*/
-	
-		
-		//Version 9 > JAVA - Implementar el try-with-resourse - para asegurarse de cerrar la conexion, JVM se encarga de cerrarla.
-		
-		final ResultSet resultSet = statement.getGeneratedKeys(); // Resultado de los id generados con la ejecucion de la Query
-		
-		try(resultSet){
-			while(resultSet.next()) {	//while para obtener el valor del id generado
-				
-				/*// Bloque de codigo para: //private void ejecutaRegistro(String nombre, String descripcion, Integer cantidad, PreparedStatement statement)  //metodo anterior usando Map se le envia las variables que tendria la data.		 
-					System.out.println(String.format(
-								"Fue insertado el producto de ID %d",
-								 resultSet.getInt(1)));//posicion 1 para saber cual id fue generado										
-			*/
-				
-				 // Bloque de codigo para: private void ejecutaRegistro(Producto producto, PreparedStatement statement)
-					producto.setId(resultSet.getInt(1));
-					System.out.println(String.format("Fue insertado el producto %s", producto));
-			
-			}
-		}
-		
-				
-		
-	/*
-		//Version 7 de JAVA - Implementar el try-with-resourse - para asegurarse de cerrar la conexion	
-		try(ResultSet resultSet = statement.getGeneratedKeys();){
-			while(resultSet.next()) {//while para obtener el valor del id generado
-				System.out.println(String.format(
-							"Fue insertado el producto de ID %d",
-							 resultSet.getInt(1)));//posicion 1 para saber cual id fue generado										
-		}
-		}
-	*/	
-		
-					
-	} 
 	
 }
 
